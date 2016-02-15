d3d5725c962354e1e5c21cc02c2f93bf1613babc
Seba <gnu.seba@outlook.com>
2016-02-15 17:19:53 +0100

    init Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,45 @@
+'use strict';
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+export class Button extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    underlayColor: "#fff",
+    style: {}
+  };
+
+  render () {
+    return (
+      <TouchableHighlight underlayColor={this.props.underlayColor}>
+        <View style={[styles.Button, this.props.style.backgroundColor ? {backgroundColor: this.props.style.backgroundColor} : null]}>
+          <Text style={[styles.ButtonText, this.props.style.color ? {color : this.props.style.color} : null]}>{this.props.text}</Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  Button: {
+    backgroundColor: "#00f",
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 4,
+  },
+  ButtonText: {
+    color: "#fff",
+  }
+});
+
