a2e645f33453d40e28588cc75427cd347aa525de
Seba <gnu.seba@outlook.com>
2016-02-17 10:59:51 +0100

    set <Navigator> as child of <Provider> directly
    without using a function

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 import React, {
   AppRegistry,
   Component,
@@ -9,17 +9,17 @@ import React, {
   TextInput,
   TouchableHighlight,
   Navigator
-} from 'react-native';
+} from "react-native";
 
 import Login from "./src/Login";
 import { AvailableRidesScene } from "./src/AvailableRides";
 import { AccountScene } from "./src/Account";
-import { Provider } from 'react-redux/native';
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./src/flux/reducer";
 import * as actionCreators from "./src/flux/actionCreators";
 import actionMiddleware from "./src/flux/actionMiddleware";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 const createStoreWithMiddleware = applyMiddleware(actionMiddleware)(createStore);
 const store = createStoreWithMiddleware(reducer);
@@ -28,11 +28,10 @@ class MatchRiderGO extends Component {
   render() {
     return (
       <Provider store={store}>
-        {() => <Navigator
-              initialRoute={{name: "Login", index: 0}}
-              renderScene={(route, navigator) => <Login/>}
-          />
-        }
+        <Navigator
+          initialRoute={{name: "Login", index: 0}}
+          renderScene={(route, navigator) => <Login/>}
+        />
       </Provider>
     );
   }
@@ -40,4 +39,4 @@ class MatchRiderGO extends Component {
 
 const styles = StyleSheet.create({});
 
-AppRegistry.registerComponent('MatchRiderGO', () => MatchRiderGO);
+AppRegistry.registerComponent("MatchRiderGO", () => MatchRiderGO);
