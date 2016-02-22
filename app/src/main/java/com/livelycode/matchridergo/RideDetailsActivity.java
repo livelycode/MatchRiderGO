1f8de549216a8db35dec2c31d02ac64c643e034e
Seba <gnu.seba@outlook.com>
2016-02-22 15:54:05 +0100

    add RideDetails activity

---
+++ app/src/main/java/com/livelycode/matchridergo/RideDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.livelycode.matchridergo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RideDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_details);
+    }
+}
