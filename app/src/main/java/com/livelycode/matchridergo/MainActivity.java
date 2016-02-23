1da6aa8258b7d656b80ee8754c6c68a1c0b3258e
Seba <gnu.seba@outlook.com>
2016-02-23 16:40:16 +0100

    start separate activities instead of fragments

--- app/src/main/java/com/livelycode/matchridergo/MainActivity.java
+++ app/src/main/java/com/livelycode/matchridergo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.livelycode.matchridergo;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.CoordinatorLayout;
@@ -16,18 +21,77 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private FragmentManager fragmentManager;
+    private Stack<Fragment> fragmentStack;
+
+    static public Stack<Activity> activityStack;
+
+    public DrawerLayout drawer;
+    public FloatingActionButton fab;
+    public NavigationView navigationView;
+    public Toolbar toolbar;
+    public ActionBarDrawerToggle toggle;
+    protected FrameLayout content;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        activityStack = new Stack<Activity>();
+        /*setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentStack = new Stack();
+
+        /*fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new BookedRidesFragment());
+        fragmentTransaction.commit();*/
+
+    }
+
+    protected void onCreateDrawer(final int layoutResID) {
+        setContentView(R.layout.activity_main);
+        content = (FrameLayout) findViewById(R.id.fragment_container);
+        getLayoutInflater().inflate(layoutResID, content, true);
+
+        setupActionBar();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    private void setupActionBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,33 +99,40 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-
-
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, new BookedRidesFragment());
-        fragmentTransaction.commit();
     }
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        fragmentStack.push(fragment);
+    }
+
 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Log.i("System.out", "test");
+        Log.i("System.out", String.valueOf(activityStack.size()));
+        Log.i("System.out", activityStack.lastElement().getLocalClassName());
+
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            /*if (fragmentStack.size() > 1) {
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                Fragment currentFragment = fragmentStack.pop();
+                Fragment previousFragment = fragmentStack.lastElement();
+                ft.replace(R.id.fragment_container, previousFragment);
+                ft.commit();
+            }*/
             super.onBackPressed();
         }
+
+        /*if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }*/
     }
 
     @Override
@@ -92,28 +163,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        if (id == R.id.nav_booked_rides) {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new BookedRidesFragment());
-            fragmentTransaction.commit();
-
-            fab.show();
-        } else if (id == R.id.nav_user_account) {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new UserAccountFragment());
-            fragmentTransaction.commit();
-
-            fab.hide();
-        } else if (id == R.id.nav_send_coupon) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Intent intent;
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+        switch (item.getItemId()) {
+            case R.id.nav_booked_rides:
+                startActivity(new Intent(this, BookedRidesActivity.class));
+                return true;
+            case R.id.nav_user_account:
+                startActivity(new Intent(this, UserAccountActivity.class));
+                return true;
+        }
+        return false;
+
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
+        //return true;
     }
 }
