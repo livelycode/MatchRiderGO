0f3aeb8d6809238d9db3a7829d34cc2ada34fbc3
Seba <gnu.seba@outlook.com>
2016-02-23 16:40:29 +0100

    destroy LoginActivity after logging in

--- app/src/main/java/com/livelycode/matchridergo/LoginActivity.java
+++ app/src/main/java/com/livelycode/matchridergo/LoginActivity.java
@@ -3,6 +3,7 @@ package com.livelycode.matchridergo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -144,7 +145,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
+        Intent intent = new Intent(this, BookedRidesActivity.class);
+        startActivity(intent);
+        finish();
+
+        /*if (mAuthTask != null) {
             return;
         }
 
@@ -188,6 +193,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
         }
+
+        */
     }
 
     private boolean isEmailValid(String email) {
