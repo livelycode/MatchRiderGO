c60b1c7e13f65cdeaaa6d63a569172a4ae55a680
Seba <gnu.seba@outlook.com>
2016-02-26 15:59:29 +0100

    create some dummy MatchRiderObjects

--- app/src/main/java/com/livelycode/matchridergo/MatchRiderObjects/Ride.java
+++ app/src/main/java/com/livelycode/matchridergo/MatchRiderObjects/Ride.java
@@ -3,15 +3,29 @@ package com.livelycode.matchridergo.MatchRiderObjects;
 import java.util.GregorianCalendar;
 
 final public class Ride {
-    private final String driverName = "Peter Hans";
-    private final String carName = "Peugot";
+    private static int numberOfRides;
+
+    public final Driver driver = new Driver();
+    private  String carName;
     private final GregorianCalendar date = new GregorianCalendar(115, 2, 29, 10, 50, 0);
-    private final String startLocation = "Heidelberg";
-    private final String destinationLocation ="Mannheim";
-    private final double duration = 20.5;
-    private final double price = 3.5;
+    public  String startLocation = "Heidelberg";
+    public  String destinationLocation = "Mannheim";
+    private  double duration;
+    private  double price;
 
     public Ride () {
+        numberOfRides++;
+
+        carName = "Peugot " + String.valueOf(numberOfRides);
+
+        if(numberOfRides % 2 == 1) {
+            startLocation = "Mannheim";
+        } else {
+            startLocation = "Heidelberg";
+        }
+
+        duration = 20 + numberOfRides;
+        price = 3.5 + numberOfRides;
     }
 }
 
