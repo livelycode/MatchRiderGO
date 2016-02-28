f8b8dd1fd05761ae1762190542497173899e39bf
Seba <gnu.seba@outlook.com>
2016-02-28 21:03:47 +0100

    implement DataBinding to RideDetailsActivity
    * http://developer.android.com/tools/data-binding/guide.html

--- app/src/main/java/com/livelycode/matchridergo/RideDetailsActivity.java
+++ app/src/main/java/com/livelycode/matchridergo/RideDetailsActivity.java
@@ -1,13 +1,25 @@
 package com.livelycode.matchridergo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.livelycode.matchridergo.databinding.ActivityRideDetailsBinding;
 
 public class RideDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ride_details);
+        //setContentView(R.layout.activity_ride_details);
+
+        Bundle bundle = getIntent().getExtras();
+        long ride_index = bundle.getLong("ride_index");
+
+        ActivityRideDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_ride_details);
+        binding.setRide(GlobalData.getInstance().getRides()[(int) ride_index]);
+
+        // Enable `Back' button in ActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
