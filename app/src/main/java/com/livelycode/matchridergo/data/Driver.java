ce3a96c2655bb5898a9597482aef1547714f9169
Seba <gnu.seba@outlook.com>
2016-02-28 21:02:06 +0100

    update Driver class
    * 3 constructors
    * 2nd constructor receiving a HashMap
    * 3rd constructor receiving a JSON object

--- app/src/main/java/com/livelycode/matchridergo/data/Driver.java
+++ app/src/main/java/com/livelycode/matchridergo/data/Driver.java
@@ -1,15 +1,70 @@
 package com.livelycode.matchridergo.data;
 
-final public class Driver {
-    private static int numberOfDrivers;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public final String name;
-    public final int score;
+import java.util.HashMap;
 
-    Driver () {
-        numberOfDrivers++;
+final public class Driver implements IMatchRiderObject  {
+    private String name;
+    private String description;
+    private String email;
+    private String id;
+    private String photo;
+    private String phone;
+    private double score;
 
-        name = "Peter Hans " + String.valueOf(numberOfDrivers);
-        score = 5;
+    public Driver(String name, String email, String photo, String description, String phone, String id, double score) {
+        this.name = name;
+        this.email = email;
+        this.photo = photo;
+        this.description = description;
+        this.phone = phone;
+        this.id = id;
+        this.score = score;
     }
+
+    public Driver(HashMap<String, Object> driverConfig) {
+        new Driver((String) driverConfig.get("name"),
+                   (String) driverConfig.get("email"),
+                   (String) driverConfig.get("photo"),
+                   (String) driverConfig.get("description"),
+                   (String) driverConfig.get("phone"),
+                   (String) driverConfig.get("id"),
+                   (double) driverConfig.get("score"));
+    }
+
+    public Driver(JSONObject json) throws JSONException, MatchRiderException {
+        String[] requiredFields = {
+                "id",
+                "name",
+                "email",
+                "photo",
+                "score",
+                "phone",
+                "description"
+        };
+
+        for (String requiredField : requiredFields) {
+            if (!json.has(requiredField)) {
+                throw new MatchRiderException("Missing required JSON field in CAR:" + requiredField);
+            }
+        }
+
+        new Driver(json.getString("name"),
+                   json.getString("email"),
+                   json.getString("photo"),
+                   json.getString("description"),
+                   json.getString("phone"),
+                   json.getString("id"),
+                   json.getDouble("score")
+        );
+    }
+
+    public String getName() { return this.name; }
+    public String getDescription() { return this.description; }
+
+    public double getScore() { return this.score; }
+
+    public String toJSONString() { return null; }
 }
