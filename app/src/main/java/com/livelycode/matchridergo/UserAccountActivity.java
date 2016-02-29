e903c4be74b8680139f459a02a141f576df445d7
Seba <gnu.seba@outlook.com>
2016-02-29 14:14:53 +0100

    move ActionBar Menu to UserAccountActivity

--- app/src/main/java/com/livelycode/matchridergo/UserAccountActivity.java
+++ app/src/main/java/com/livelycode/matchridergo/UserAccountActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class UserAccountActivity extends MainActivity {
@@ -27,5 +29,27 @@ public class UserAccountActivity extends MainActivity {
     public void onBackPressed() {
         // TODO Auto-generated method stub
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
